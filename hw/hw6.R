e48d0c3804ed3109869eb60ef53765ab5b4ba341
yos <yosefluzon9@gmail.com>
2018-05-18 13:39:45 +0300

    hw6.1

--- hw/hw6.R
+++ hw/hw6.R
@@ -1,3 +1,30 @@
 spam<-read.csv('spamhw.CSV', stringsAsFactors = FALSE)
 View(spam)
 str(spam)
+spam$category <- as.factor(spam$category)
+
+install.packages('tm')
+library(tm)
+install.packages('wordcloud')
+library(wordcloud)
+install.packages('e1071')
+library(wordcloud)
+
+install.packages('NLP')
+library('NLP')
+install.packages('RColorBrewer')
+library(RColorBrewer)
+spam_corpus <- Corpus(VectorSource(spam$text))
+print(spam_corpus)
+inspect(spam_corpus[1:2])
+spam_corpus[[1]][[1]]
+spam_corpus[[2]][[2]]
+#הסרת סימני פיסוק
+clean_corpus <- tm_map(spam_corpus, removePunctuation)
+#הסרת רווחים כפולים
+clean_corpus <- tm_map(clean_corpus, stripWhitespace)
+clean_corpus <- tm_map(clean_corpus, removeWords, stopwords())
+clean_corpus <- tm_map(clean_corpus, stripWhitespace)
+dtm <- DocumentTermMatrix(clean_corpus)
+dim(dtm)
+clean_corpus[[1]][[1]]
